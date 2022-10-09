161f22b4170c270cc926eb2ac200d8e60b3e4f2a
Mrugapatel <107723608+Mrugapatel1007@users.noreply.github.com>
2022-10-09 15:09:55 -0500

    change

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.scss';
+import Education from './pages/Education';
+import Me from './pages/Me';
+import Projects from './pages/Projects';
+import Work from './pages/Work';
+
+function App() {
+  return (
+    <div className="App">
+      <Education/>
+      <Me/>
+      <Projects/>
+      <Work/>
+    </div>
+  );
+}
+
+export default App;

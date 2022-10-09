ba589428a064dd7654c08e91559bfbb670a5cb1a
Mrugapatel <107723608+Mrugapatel1007@users.noreply.github.com>
2022-10-09 17:14:58 -0500

    change

---
+++ src/pages/Education/Education.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import { SectionTitle, Paragraph } from "../../styles";
+import { EducationItem, Institution, Degree } from "./styles";
+
+const Education = ({ user }) => {
+  return (
+    <Layout user={user}>
+      <div>
+        <SectionTitle>Education</SectionTitle>
+        <ul>
+          {user.education.map((education, i) => (
+            <EducationItem key={i}>
+              <Institution>Software Developer</Institution>
+              <div>
+                <Degree>
+                  Full Stack Web Developer, Winnipeg,MB
+                </Degree>{" "}
+                <span> &sdot; </span>
+                <span>
+                  july 2021 to April 2022
+                </span>
+              </div>
+              <Paragraph>
+                {education.description.replace("\n\n", "\n")}
+              </Paragraph>
+            </EducationItem>
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  );
+};
+
+export default Education;
